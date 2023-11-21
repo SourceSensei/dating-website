8a2ed539a64278864cd8457b31a153adc44cc7f6
SourceSensei <source.sensei1205@gmail.com>
2023-11-21 21:09:17 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../styles/global.css';
+	import Nav from '../components/Nav.svelte';
+	import Hero from '../components/Hero.svelte';
+	import Steps from '../components/Steps.svelte';
+	import Description from './../components/Description.svelte';
+	import Faq from '../components/Faq.svelte';
+	import Contact from '../components/Contact.svelte';
+	import Footer from '../components/Footer.svelte';
+</script>
+
+<Nav />
+<Hero />
+<Steps />
+<Description />
+<Faq />
+<Contact />
+<Footer />
